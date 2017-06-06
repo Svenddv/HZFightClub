85643d186481e44e9aa3a511eb96cf03fd1b88c0
Svenddv <Svenddv@gmail.com>
2017-06-06 23:58:42 +0200

    functies voor animaties toegevoegd

--- main.js
+++ main.js
@@ -2,17 +2,15 @@ var canvas;
 var ctx;
 var background = new Image();
 background.src = "./assets/images/background.gif";
-var playerone;
-var x = 0;
-var y = 0;
-function gameloop() {
-    requestAnimationFrame(gameloop);
+var img = new Image();
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
     keyInput.inputLoop();
     ctx.drawImage(background, 250, 100);
     ctx.save();
-    ctx.translate(x, y);
-    ctx.drawImage(playerone, 250, 150);
+    ctx.translate(this.x, this.y);
     ctx.restore();
+    sprite.draw();
 }
 var KeyboardInput = (function () {
     function KeyboardInput() {
@@ -48,19 +46,133 @@ var KeyboardInput = (function () {
     }
     return KeyboardInput;
 }());
+var cFrame = (function () {
+    function cFrame(x, y, w, h, ox, oy) {
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
+        if (w === void 0) { w = 1; }
+        if (h === void 0) { h = 1; }
+        if (ox === void 0) { ox = 0; }
+        if (oy === void 0) { oy = 0; }
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.ox = ox;
+        this.oy = oy;
+    }
+    cFrame.prototype.set = function (x, y) {
+        this.x = x;
+        this.y = y;
+    };
+    return cFrame;
+}());
+var cTextureAtlas = (function () {
+    function cTextureAtlas(atlasName, loadCallback) {
+        var _this = this;
+        this.frames = {};
+        this.taLoadComplete = false;
+        this.image = new Image();
+        this.atlasName = "";
+        this._imageFile = "";
+        this._jsonFile = "";
+        this._loadJSON = function () {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                    _this.image.onload = _this.onImageLoad;
+                    _this.image.onerror = _this.onImageError;
+                    _this.image.src = _this._imageFile;
+                    _this._onRead(JSON.parse(xhr.responseText));
+                }
+                else {
+                    _this._onError(xhr);
+                }
+            };
+            xhr.open("GET", _this._jsonFile, true);
+            xhr.send();
+        };
+        this._onRead = function (data) {
+            var temp_frame;
+            for (var frame_name in data.frames) {
+                var sprite_data = data.frames[frame_name];
+                temp_frame = new cFrame(sprite_data.frame.x, sprite_data.frame.y, sprite_data.frame.w, sprite_data.frame.h, sprite_data.spriteSourceSize.x, sprite_data.spriteSourceSize.y);
+                _this.frames[frame_name] = temp_frame;
+            }
+        };
+        this._onError = function (xhr) {
+            console.log("FAILED TO LOAD ATLAS: " + _this._jsonFile);
+        };
+        this.onImageLoad = function () {
+            _this.taLoadComplete = true;
+            _this._loadCallback();
+        };
+        this.onImageError = function () {
+            console.log("IMAGE LOAD ERROR");
+        };
+        this.containsFrame = function (frameName) {
+            if (_this.frames[frameName] !== undefined &&
+                _this.frames[frameName] !== null) {
+                return true;
+            }
+            return false;
+        };
+        this.atlasName = atlasName;
+        this._imageFile = atlasName;
+        this._jsonFile = atlasName.replace(".png", "") + ".json";
+        this._loadCallback = loadCallback;
+        this._loadJSON();
+    }
+    return cTextureAtlas;
+}());
+var cAnimatedSprite = (function () {
+    function cAnimatedSprite(x, y, frame_count, atlas, sequence_name) {
+        var _this = this;
+        this.x = 0;
+        this.y = 0;
+        this.frameCount = 0;
+        this.sequenceName = "";
+        this.currentFrame = 0;
+        this.draw = function () {
+            _this.currentFrame++;
+            if (_this.currentFrame >= _this.frameCount) {
+                _this.currentFrame = 0;
+            }
+            ctx.save();
+            ctx.translate(_this.x, _this.y);
+            ctx.drawImage(_this.atlas.image, _this.atlas.frames[_this.getFrameString()].x, _this.atlas.frames[_this.getFrameString()].y, _this.atlas.frames[_this.getFrameString()].w, _this.atlas.frames[_this.getFrameString()].h, _this.atlas.frames[_this.getFrameString()].ox, _this.atlas.frames[_this.getFrameString()].oy, _this.atlas.frames[_this.getFrameString()].w, _this.atlas.frames[_this.getFrameString()].h);
+            ctx.restore();
+        };
+        this.getFrameString = function () {
+            if (_this.currentFrame > 9) {
+                return _this.sequenceName + _this.currentFrame.toString() + ".png";
+            }
+            return _this.sequenceName + "0" + _this.currentFrame.toString() + ".png";
+        };
+        this.x = x;
+        this.y = y;
+        this.frameCount = frame_count;
+        this.atlas = atlas;
+        this.sequenceName = sequence_name;
+    }
+    return cAnimatedSprite;
+}());
 var keyInput;
+var atlas;
+var sprite;
 window.onload = function () {
+    img = document.getElementById('spaceship');
     canvas = document.getElementById('cnvs');
     ctx = canvas.getContext("2d");
     keyInput = new KeyboardInput();
-    playerone = document.getElementById('playerone');
+    atlas = new cTextureAtlas("images/atlas.png", gameLoop);
+    sprite = new cAnimatedSprite(100, 100, 35, atlas, "ryustand");
     keyInput.addKeycodeCallback(37, walkLeft);
     keyInput.addKeycodeCallback(39, walkRight);
-    gameloop();
 };
 function walkLeft() {
-    x -= 2;
+    sprite.x -= 2;
 }
 function walkRight() {
-    x += 2;
+    sprite.x += 2;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,37 +2,25 @@ var canvas: HTMLCanvasElement;
 var ctx: CanvasRenderingContext2D;
 var background = new Image();
 background.src = "./assets/images/background.gif";
-var playerone: HTMLImageElement;
-var x: number = 0;
-var y: number = 0;
+var img: HTMLImageElement = new Image();
 
 
-// class Point {
-//     public x: number= 0;
-//     public y: number= 0;
-//     constructor(x: number = 0, y: number = 0) {
-//         this.x = x;
-//         this.y = y;
-//     }
-// }
 
-function gameloop() {
-    requestAnimationFrame(gameloop);
+function gameLoop(): void {
+    requestAnimationFrame(gameLoop);
     keyInput.inputLoop();
+   
+//    ctx.fillStyle = "black";
+//    ctx.fillRect(0, 0, 1280, 720);
     ctx.drawImage(background,250,100);
     ctx.save();
-    ctx.translate(x, y);
-    ctx.drawImage(playerone, 250, 150);
+    ctx.translate(this.x, this.y);
     ctx.restore();
+   
+   sprite.draw();
 }
 
-interface iShape {
-    draw(): void;
-    x: number;
-    y: number;
-    color: string;
-    lineWidth: number;
-}
+
 
 class KeyboardInput {
     public keyCallback: { [keycode: number]: () => void; } = {};
@@ -72,22 +60,166 @@ class KeyboardInput {
     }
 }
 
+class cFrame {
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+
+    ox: number;
+    oy: number
+    constructor(x: number = 0, y: number = 0, w: number = 1, h: number = 1, ox: number = 0, oy: number = 0) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.ox = ox;
+        this.oy = oy;
+    }
+
+    set(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
 
 
+
+
+class cTextureAtlas {
+    public frames: { [index: string]: cFrame } = {};
+    public taLoadComplete: boolean = false;
+    public image: HTMLImageElement = new Image();
+    public atlasName: string = "";
+    private _imageFile: string = "";
+    private _jsonFile: string = "";
+    private _loadCallback: () => void;
+
+    constructor(atlasName: string, loadCallback: () => void) {
+        this.atlasName = atlasName;
+        this._imageFile = atlasName;
+        this._jsonFile = atlasName.replace(".png", "") + ".json";
+
+        this._loadCallback = loadCallback;
+        this._loadJSON();
+
+    }
+
+    protected _loadJSON = () => {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                this.image.onload = this.onImageLoad;
+                this.image.onerror = this.onImageError;
+                this.image.src = this._imageFile;
+
+                this._onRead(JSON.parse(xhr.responseText));
+            }
+            else {
+                this._onError(xhr);
+            }
+        };
+        xhr.open("GET", this._jsonFile, true);
+        xhr.send();
+    }
+
+    protected _onRead = (data: any) => {
+        var temp_frame: cFrame;
+
+        for (var frame_name in data.frames) {
+            var sprite_data: any = data.frames[frame_name];
+
+            temp_frame = new cFrame(sprite_data.frame.x,
+                sprite_data.frame.y,
+                sprite_data.frame.w,
+                sprite_data.frame.h,
+                sprite_data.spriteSourceSize.x,
+                sprite_data.spriteSourceSize.y,
+                );
+
+            this.frames[frame_name] = temp_frame;
+        }
+
+    }
+    protected _onError = (xhr: XMLHttpRequest) => {
+        console.log("FAILED TO LOAD ATLAS: " + this._jsonFile);
+    }
+
+    private onImageLoad = () => {
+        this.taLoadComplete = true;
+        this._loadCallback();
+    }
+
+    private onImageError = () => {
+        console.log("IMAGE LOAD ERROR");
+    }
+
+    public containsFrame = (frameName: string): boolean => {
+        if (this.frames[frameName] !== undefined &&
+            this.frames[frameName] !== null) {
+            return true;
+        }
+        return false;
+    }
+}
+
+class cAnimatedSprite {
+    public x: number = 0;
+    public y: number = 0;
+    public frameCount: number = 0;
+    public atlas: cTextureAtlas;
+    public sequenceName: string = "";
+
+    public currentFrame = 0;
+
+    constructor(x: number, y: number, frame_count: number, atlas: cTextureAtlas, sequence_name: string) {
+        this.x = x;
+        this.y = y;
+        this.frameCount = frame_count;
+        this.atlas = atlas;
+        this.sequenceName = sequence_name;
+    }
+
+    public draw = (): void => {
+        this.currentFrame++;
+        if (this.currentFrame >= this.frameCount) {
+            this.currentFrame = 0;
+        }
+
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.drawImage(this.atlas.image,
+            this.atlas.frames[this.getFrameString()].x, this.atlas.frames[this.getFrameString()].y,
+            this.atlas.frames[this.getFrameString()].w, this.atlas.frames[this.getFrameString()].h,
+            this.atlas.frames[this.getFrameString()].ox, this.atlas.frames[this.getFrameString()].oy,
+            this.atlas.frames[this.getFrameString()].w, this.atlas.frames[this.getFrameString()].h);
+        ctx.restore();
+    }
+
+    public getFrameString = (): string => {
+        if (this.currentFrame > 9) {
+            return this.sequenceName + this.currentFrame.toString() + ".png";
+        }
+        return this.sequenceName + "0" + this.currentFrame.toString() + ".png";
+    }
+}
 
 var keyInput: KeyboardInput;
-
-
+var atlas: cTextureAtlas;
+var sprite: cAnimatedSprite;
 
 
 
 
 window.onload = () => {
-   
+    img = <HTMLImageElement>document.getElementById('spaceship')
     canvas = <HTMLCanvasElement>document.getElementById('cnvs');
     ctx = canvas.getContext("2d");
     keyInput = new KeyboardInput();
-    playerone = <HTMLImageElement>document.getElementById('playerone') 
+    atlas = new cTextureAtlas("images/atlas.png", gameLoop);
+    sprite = new cAnimatedSprite(100, 100, 35, atlas, "ryustand");
+    
    
     // PRESS LEFT ARROW OR 'A' KEY
     keyInput.addKeycodeCallback(37, walkLeft);
@@ -95,13 +227,13 @@ window.onload = () => {
     // PRESS RIGHT ARROW OR 'D' KEY
     keyInput.addKeycodeCallback(39, walkRight);
     
-    gameloop();
+    
 }
 
 function walkLeft(): void {
-    x -= 2;
+    sprite.x -= 2;
 }
 
 function walkRight(): void {
-    x += 2;
+    sprite.x += 2;
 }
